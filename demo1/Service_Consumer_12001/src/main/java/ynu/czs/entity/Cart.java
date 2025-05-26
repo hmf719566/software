bbc604a7eef501d47cfc361ebf437de4b32b9505
hmf719566 <162271279+hmf719566@users.noreply.github.com>
2025-05-26 14:29:21 +0800

    Add files via upload

---
+++ demo1/Service_Consumer_12001/src/main/java/ynu/czs/entity/Cart.java
@@ -0,0 +1,16 @@
+package ynu.czs.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+    private User user;
+    private List<String> goodList;
+
+}
---
+++ demo1/Service_Consumer_12001/src/main/java/ynu/czs/entity/User.java
@@ -0,0 +1,16 @@
+package ynu.czs.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Integer userId;
+    private String userName;
+    private String password;
+
+
+}
